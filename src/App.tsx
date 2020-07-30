fdeee6e84ec32b2b1a04767412d7cc37f3bd8ba7
Sam Wight <samuelwight@gmail.com>
2020-07-29 21:58:25 -0500

    Upload.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { firebaseConfig } from "config/firebase";
+import {
+  FirebaseAppProvider,
+  useFirestore,
+  useFirestoreDocData,
+  AuthCheck,
+  useUser,
+} from "reactfire";
+
+interface HabitType {
+  visibility: string;
+  current_version: firebase.firestore.DocumentReference;
+}
+
+interface VersionType {
+  create_date: {
+    seconds: number;
+    nanoseconds: number;
+  };
+  description: string;
+  name: string;
+}
+
+const Example = () => {
+  const habitRef = useFirestore()
+    .collection("habits")
+    .doc("X0PGnSRtValC3iMPA9n4");
+  const user = useUser();
+
+  const habit: HabitType = useFirestoreDocData(habitRef);
+  const version: VersionType = useFirestoreDocData(habit.current_version);
+  console.log(user);
+  return (
+    <div>
+      <h1>{version.name}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <div className="App">
+        <h1>This works!</h1>
+        <Suspense fallback={<h1>Loading</h1>}>
+          <Example />
+        </Suspense>
+      </div>
+    </FirebaseAppProvider>
   );
 }
 
---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT66vUaHq-Dn2fnU1yl0sY2YWMkPB2Iv8",
+  authDomain: "wont-68ff4.firebaseapp.com",
+  databaseURL: "https://wont-68ff4.firebaseio.com",
+  projectId: "wont-68ff4",
+  storageBucket: "wont-68ff4.appspot.com",
+  messagingSenderId: "476887336548",
+  appId: "1:476887336548:web:1782d75854ff2e0fe70171",
+  measurementId: "G-NBL9BXTZXD",
+};
